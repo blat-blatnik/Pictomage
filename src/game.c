5d4769614ccc4fcf5cafaee204034a36ba1cc3ef
blat-blatnik <blatblatnik@protonmail.com>
2022-01-21 17:39:02 +0100

    Updated gitignore.

--- src/game.c
+++ src/game.c
@@ -2,35 +2,8 @@
 
 Texture2D texture;
 
-#include <time.h>
 void GameInit(void)
 {
-    Random r = SeedRandom(time(NULL));
-    u64 bit_counts[20] = {0};
-    u64 total_count = 0;
-    for (u64 i = 0;; ++i)
-    {
-        u32 r = (u32)RandomInt(&r, 0, 1 << 20);
-        total_count++;
-        u32 bit = 1;
-        for (int j = 0; j < 20; ++j)
-        {
-            if (r & bit)
-                bit_counts[j]++;
-            bit <<= 1;
-        }
-        if (i % 1000000000 == 0)
-        {
-            for (int i = 19; i >= 0; --i)
-                printf("[%4d] ", i);
-            printf("\n");
-            for (int i = 19; i >= 0; --i)
-                printf("%.4f ", bit_counts[i] / (double)total_count);
-            printf("\n\n");
-        }
-    }
-
-    TraceLog(LOG_INFO, "Done");
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(900, 600, "Raylib Test");
     texture = LoadTexture("res/test.png");

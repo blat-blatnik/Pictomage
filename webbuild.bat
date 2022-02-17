013e10b7a3406923d45cd13392f599418396f413
blat-blatnik <blatblatnik@protonmail.com>
2022-02-18 00:52:31 +0100

    Reduced memory usage on web.

--- webbuild.bat
+++ webbuild.bat
@@ -14,7 +14,7 @@ rem # Collect all .c files for compilation into the 'input' variable.
 for /f %%a in ('forfiles /s /m *.c /c "cmd /c echo @relpath"') do set input=!input! "%%~a"
 
 rem # Compile and link
-call emcc -o bin/web/index.html -Wall -L./lib -lraylib_web -s USE_GLFW=3 -s TOTAL_MEMORY=134217728 --shell-file webshell.html --preload-file res %input%
+call emcc -o bin/web/index.html -Wall -L./lib -lraylib_web -s USE_GLFW=3 -s TOTAL_MEMORY=16777216 --shell-file webshell.html --preload-file res %input%
 
 rem # Make sure .data is smaller than 32MB
 set /a size=0
